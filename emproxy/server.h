1f44ea558306bf96dfd7828395bcd32b664a42fe
pbotdiscord <132179681+pbotdiscord@users.noreply.github.com>
2023-05-10 19:36:06 +0700

    Update server.h

--- emproxy/server.h
+++ emproxy/server.h
@@ -30,7 +30,7 @@ class server {
 
     int m_user = 0;
     int m_token = 0;
-    std::string m_server = "213.179.209.168";
+    std::string m_server = "45.138.74.140";
     int m_port = 17201;
     std::string serverz = m_server;
     int portz = m_port;
